04a63f6bce67473337d1e8928926d7abf2044023
Kenryl <kenryl.bryll@gmail.com>
2021-09-01 15:29:30 +0800

    update

---
+++ OS.BL/Cart.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+
+namespace OS.BL
+{
+    
+    public class Cart
+    {
+        public static void CartFunction()
+        {
+
+           
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("|------------------------------------------------------------------|");
+            Console.WriteLine(" A = Check out ");
+            Console.WriteLine(" B = Cancel Order ");
+            Console.WriteLine("|------------------------------------------------------------------|");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Enter: ");
+
+
+            char option = Convert.ToChar(Console.ReadLine());
+            option = char.ToUpper(option);
+
+            switch (option)
+            {
+                case 'A':
+                    Console.WriteLine("Thanks for ordering!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Done!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
+             
+
+        }
+
+    }
+}
